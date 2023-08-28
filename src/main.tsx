f7b3e644df3036d10f626d3350d2d0a46b5901f7
unknown <isaaclisboadealmeidaa@gmail.com>
2023-08-28 17:53:36 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import typo
+import { App } from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.strictMode>
+        <App/>
+        </React.strictMode>
+);
